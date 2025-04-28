3799bcf7824199693711ac4aa5474f8c67032e24
Kiruya Yuuki <cldus26@gmail.com>
2025-04-28 22:48:59 +0900

    Revert "Feat/player"

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -4,20 +4,7 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    
-
     public Animator anim;
-    public float moveSpeed;
-    public float rotateSpeed;
-
-    // 유니티에서 Ground 레이어 추가하기
-    public LayerMask groundLayer;
-    public float groundCheckDistance = 0.2f;
-
-    public bool IsGrounded()
-    {
-        return Physics.Raycast(transform.position, Vector3.down, groundCheckDistance, groundLayer);
-    }
 
     // 나중에 주석 없애도 됩니다.
 }
