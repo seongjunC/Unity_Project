8edc7e7411e07983a072c819a6b367bb51f64899
HSD06040 <tinto0604@gmail.com>
2025-04-30 16:37:37 +0900

    [Feat] 골드 데이터 추가

--- Assets/Scripts/Managers/GameManager.cs
+++ Assets/Scripts/Managers/GameManager.cs
@@ -1,8 +1,20 @@
-using System.Collections;
-using System.Collections.Generic;
+using System;
 using UnityEngine;
 
 public class GameManager : Singleton<GameManager>
 {
-    
+    private int _curGold;
+    public int gold { get { return _curGold; }  private set { _curGold = value; OnGoldChanged?.Invoke(_curGold); } }
+
+    public event Action<int> OnGoldChanged;
+
+    public void AddGold(int amount) => gold += amount;
+
+    public void RemoveGold(int amount)
+    {
+        if (gold - amount < 0)
+            return;
+
+        gold -= amount;
+    }
 }
